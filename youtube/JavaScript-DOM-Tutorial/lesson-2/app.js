c7d2efd2ebd012d879331e99ae7f97e0e78baae2
MehdiDonk <mehdi.naqi@gmail.com>
2019-11-11 21:15:43 +0100

    update list book tuto

--- youtube/JavaScript-DOM-Tutorial/lesson-2/app.js
+++ youtube/JavaScript-DOM-Tutorial/lesson-2/app.js
@@ -1,4 +1,47 @@
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
+const list = document.querySelector('#book-list ul');
 
-console.log(search, bookList);
+// delete books
+list.addEventListener('click', function(e) {
+	console.log(e.target);
+	if (e.target.className == 'delete') {
+		const li = e.target.parentElement;
+		li.remove();
+	}
+});
+
+// add book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e) {
+	e.preventDefault();
+	const value = addForm.querySelector('input[type="text"]').value;
+	console.log(value);
+
+	// create elements
+	const li = document.createElement('li');
+	const bookName = document.createElement('span');
+	const deleteBtn = document.createElement('span');
+
+	// add content
+	deleteBtn.textContent = 'delete';
+	bookName.textContent = value;
+
+	// add class
+	deleteBtn.classList.add('delete');
+	bookName.classList.add('name');
+
+	// append to document
+	li.appendChild(bookName);
+	li.appendChild(deleteBtn);
+	list.appendChild(li);
+});
+
+// hide book
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e) {
+	if (hideBox.checked) {
+		list.style.display = 'none';
+	} else {
+		list.style.display = 'block';
+	}
+});
