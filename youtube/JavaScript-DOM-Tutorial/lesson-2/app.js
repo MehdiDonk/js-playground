06956a53aeee6c6bc44a6b3e62888e5c4334c716
MehdiDonk <mehdi.naqi@gmail.com>
2019-11-12 20:18:03 +0100

    Tab nav

--- youtube/JavaScript-DOM-Tutorial/lesson-2/app.js
+++ youtube/JavaScript-DOM-Tutorial/lesson-2/app.js
@@ -62,3 +62,19 @@ searchBar.addEventListener('keyup', function(e) {
 		}
 	});
 });
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e) {
+	if (e.target.tagName == 'LI') {
+		const targetPanel = document.querySelector(e.target.dataset.target);
+		Array.from(panels).map((panel) => {
+			if (panel == targetPanel) {
+				panel.classList.add('active');
+			} else {
+				panel.classList.remove('active');
+			}
+		});
+	}
+});
